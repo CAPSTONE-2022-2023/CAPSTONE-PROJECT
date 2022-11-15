c907fd47ca6073e9cc3aa4a2543642b36b455850
Gaurav Saini <gauravsaini@Gauravs-MBP.ht.home>
2022-11-14 20:37:48 -0500

    added login page

--- src/components/RegisterAccountPage.js
+++ src/components/RegisterAccountPage.js
@@ -1,13 +1,30 @@
-import React from 'react';
-// import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import '../css/LoginPage.css';
 
-export default function ErrorPage() 
-{
+const RegisterPage = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		// TODO
+		console.log('username', username);
+		console.log('password', password);
+	}
+
 	return (
-
-		<div>
-			<h1>THIS IS THE REGISTER ACCOUNT PAGE!</h1>
+		<div id="login-container">
+			<h1 className="title">Bank App</h1>
+			<form id="login" onSubmit={(e) => onSubmit(e)}>
+				<label htmlFor="username">Username: </label>
+				<input id="username" type="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+				<label htmlFor="username">Password: </label>
+				<input id="password" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+				<input type="submit" value="Login" />
+			</form>
 		</div>
+	)
+};
 
-  	);
-}
+export default RegisterPage;
