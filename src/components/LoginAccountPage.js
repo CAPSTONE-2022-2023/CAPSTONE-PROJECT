bc9e6793e46b0b8d629bf324d3ee0b2a4ff5c4e8
Gaurav3023 <44793171+Gaurav3023@users.noreply.github.com>
2022-12-05 19:46:45 -0500

    added validation to the login page

--- src/components/LoginAccountPage.js
+++ src/components/LoginAccountPage.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../css/LoginPage.css';
 
 const LoginPage = () => {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [error, setError] = useState();
+	const navigate = useNavigate();
+
 
 	const onSubmit = (e) => {
 		e.preventDefault();
 
 		// TODO
-		console.log('username', username);
-		console.log('password', password);
+		if (username === "BOB" && password === "1234") {
+			navigate("/bankAccountOverview")
+
+		}
+
+		else {
+			setError("Incorrect username or password")
+		}
+
 	}
 
 	return (
@@ -19,12 +29,15 @@ const LoginPage = () => {
 			<h1 className="title">Login to the Bank App</h1>
 			<form id="login" onSubmit={(e) => onSubmit(e)}>
 				<label htmlFor="username">Username: </label>
-				<input id="username" type="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+				<input id="username" type="username" name="username" placeholder="Enter Username" value={username} onChange={(e) => setUsername(e.target.value)} />
 				<label htmlFor="username">Password: </label>
-				<input id="password" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-				<Link to = "/bankAccountOverview"> <input type="submit" value="Login" /> </Link>
+				<input id="password" type="password" name="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+				<input type="submit" value="Login" />
 				<div>
 					Don't have an account? <Link to="/register">Sign Up</Link>
+				</div>
+				<div>
+					{error}
 				</div>
 			</form>
 		</div>
